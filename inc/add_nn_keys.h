ae5c345e1678ce72444235f796ac43cdafb5d188
Ramesh Subramonian <ramesh.subramonian@target.com>
2025-01-13 01:38:32 +0000

    ok

--- inc/add_nn_keys.h
+++ inc/add_nn_keys.h
@@ -3,12 +3,12 @@ add_nn_keys(
     char **keys, // [nK]
     const qtype_t * const qtypes, // [nK]
     const uint32_t *const widths, // nK] 
-    bool *has_nulls, // [nK]
-    bool *is_load, // [nK]
+    const bool * const has_nulls, // [nK]
+    const bool * const is_load, // [nK]
     uint32_t nK,
     // output below 
     char ***ptr_all_keys, // [all_nK]
-    bool **ptr_all_qtypes, // [all_nK]
+    qtype_t **ptr_all_qtypes, // [all_nK]
     uint32_t **ptr_all_widths, // [all_nK]
     uint32_t *ptr_all_nK
     );
