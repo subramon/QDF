22faf31ea159f80f38100a539b2257fb8eabe704
Ramesh Subramonian <ramesh.subramonian@target.com>
2025-05-10 21:24:28 +0000

    ok

--- src/qdf_add_col_to_df.c
+++ src/qdf_add_col_to_df.c
@@ -11,11 +11,7 @@
 #include "qdf_add_col_to_df.h"
 
 int
-<<<<<<< HEAD
-qdf_add_col_to_df(
-=======
 add_col_to_df(
->>>>>>> 8b19c14aaaedc7bfae3693856510625aada06477
     QDF_REC_TYPE *src,
     const char * const col, // name of new column
     QDF_REC_TYPE *add, // column to be added 
@@ -26,17 +22,12 @@ add_col_to_df(
   mcr_chk_non_null(src, -1); 
   mcr_chk_null(dst, -1); 
   char **cols = NULL; uint32_t n_cols = 0;
-<<<<<<< HEAD
-  qtype_t *qtypes = NULL; uint32_t chk_n;
-  uint32_t *widths = NULL; 
-=======
   qtype_t *qtypes = NULL; 
   uint32_t *widths = NULL; 
   char **new_cols = NULL; uint32_t new_n_cols = 0;
   qtype_t *new_qtypes = NULL; 
   uint32_t *new_widths = NULL; 
   uint32_t chk_n;
->>>>>>> 8b19c14aaaedc7bfae3693856510625aada06477
 
   uint32_t sz_rows = get_obj_arr_size(src->data); 
   uint32_t n_rows  = get_obj_arr_len(src->data); 
@@ -47,13 +38,6 @@ add_col_to_df(
   }
   status = get_qtypes_as_array(src, &qtypes, &chk_n); cBYE(status);
   if ( chk_n != n_cols ) { go_BYE(-1); }
-<<<<<<< HEAD
-  status = get_widths_as_array(src, &qtypes, &chk_n); cBYE(status);
-  if ( chk_n != n_cols ) { go_BYE(-1); }
-
-  status = make_empty_data_frame(cols, n_cols, qtypes, widths, 
-      sz_rows, dst); // TODO P3 Should this be n_rows ?
-=======
   status = get_widths_as_array(src, &widths, &chk_n); cBYE(status);
   if ( chk_n != n_cols ) { go_BYE(-1); }
   // add to create new meta data 
@@ -80,7 +64,6 @@ add_col_to_df(
   // Check length of new_col versus length of existing 
   status = make_empty_data_frame(new_cols, new_n_cols, new_qtypes, 
       new_widths, sz_rows, dst); // TODO P3 Should this be n_rows ?
->>>>>>> 8b19c14aaaedc7bfae3693856510625aada06477
   cBYE(status);
   // add old columns
   for ( uint32_t i = 0; i < n_cols; i++ ) { 
@@ -94,11 +77,8 @@ BYE:
   free_2d_array(&cols, n_cols);
   free_if_non_null(qtypes);
   free_if_non_null(widths);
-<<<<<<< HEAD
-=======
   free_2d_array(&new_cols, new_n_cols);
   free_if_non_null(new_qtypes);
   free_if_non_null(new_widths);
->>>>>>> 8b19c14aaaedc7bfae3693856510625aada06477
   return status;
 }
